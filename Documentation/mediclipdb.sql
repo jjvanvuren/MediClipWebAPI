4af551c397bda1de0699007625e05d723cda8a86
Karl Foley <c3274832@uon.edu.au>
2018-10-20 07:23:27 +1100

    Name Change

--- Documentation/mediclipdb.sql
+++ Documentation/mediclipdb.sql
@@ -42,7 +42,7 @@ CREATE TABLE Notes(
 	NoteID						INT 			NOT NULL CHECK (NoteID > 0 AND NoteID <= 9999),
 	PatientID					INT				NOT NULL CHECK (WardID > 0 AND WardID <= 9999),
 	NoteTitle					VARCHAR(50)		NOT NULL,
-	Notes						VARCHAR(500)	NOT NULL,
+	Note						VARCHAR(500)	NOT NULL,
 	Picture						VARCHAR(100)	NOT NULL,
 	PRIMARY KEY (NoteID, PatientID),
 	FOREIGN KEY(PatientID)		REFERENCES Patient (PatientID)	ON UPDATE CASCADE ON DELETE CASCADE,
