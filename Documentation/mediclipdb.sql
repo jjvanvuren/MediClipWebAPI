94b50c989b78f5875c83ee00ebfc92a8624d8b47
Karl Foley <c3274832@uon.edu.au>
2018-10-20 08:06:40 +1100

    Updated Tables to Match
    Added tables to match classes
    also added password to nurses, BedNumber to Patient and Wardtitle to Ward

--- Documentation/mediclipdb.sql
+++ Documentation/mediclipdb.sql
@@ -2,12 +2,14 @@
 CREATE TABLE Nurse (
 	NurseID						INT				PRIMARY KEY CHECK (NurseID > 0 AND NurseID <= 9999),
 	FirstName					VARCHAR(15)		NOT NULL,
-	LastName					VARCHAR(15)		NOT NULL
+	LastName					VARCHAR(15)		NOT NULL,
+	Password					VARCHAR(30)		NOT NULL
 )
 
 --Create the Ward table
 CREATE TABLE Ward (
 	WardID						INT				PRIMARY KEY CHECK (WardID > 0 AND WardID <= 9999),
+	WardTitle					VARCHAR(15)		NOT NULL,\
 	Location					VARCHAR(15)		NOT NULL,
 	Description					VARCHAR(150)	NOT NULL
 )
@@ -23,6 +25,7 @@ CREATE TABLE Patient (
 	Dob							DATE			NOT NULL,
 	Sex							VARCHAR(7)		NOT NULL,
 	Dosage						VARCHAR(150)	NOT NULL,
+	BedNumber					INT(2)			CHECK (BedNumber > 0 AND WardID <= 30),
 	FOREIGN KEY(WardID)		REFERENCES	Ward (WardID) ON UPDATE CASCADE ON DELETE CASCADE
 )
 
